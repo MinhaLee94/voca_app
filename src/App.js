4f83ad816cd8867593323f311c45afac7ba0dec6
MinhaLee94 <shjune1994@gmail.com>
2021-09-14 00:31:23 -0400

    Day and DayList component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Day from './components/Day';
+import DayList from './components/DayList';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DayList />
+      <Day />
     </div>
   );
 }
